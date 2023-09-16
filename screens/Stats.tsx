f00eb674f0d0641408591596e8aa6b3d50796caf
Anil Tuncel <anil.tuncel@epfl.ch>
2023-09-16 21:51:51 +0200

    initial Stats screen that count n_completed tasks

--- screens/Stats.tsx
+++ screens/Stats.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { AppContext } from '../src/store/appContext';
 
 export default function Stats() {
+    const { tasksList } = React.useContext(AppContext);
+    const completedTasksCount = tasksList.length;
 
     return (
-        <Text>Stats</Text>
+        <View style={styles.container}>
+            <Text style={styles.header}>Stats</Text>
+            <Text style={styles.info}>Total tasks ever completed: {completedTasksCount}</Text>
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20
+    },
+    header: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20
+    },
+    info: {
+        fontSize: 18
+    }
 });
