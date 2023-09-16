4ad8f4586c5ebb2b0c5630aa469f8773abea69cc
Anil Tuncel <anil.tuncel@epfl.ch>
2023-09-16 16:22:21 +0200

    add checkbox for tasks

--- screens/Tasks.tsx
+++ screens/Tasks.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import Checkbox from 'expo-checkbox';
 
 
 export default function Tasks() {
     const [task, setTask] = useState<string>('');
     const [tasks, setTasks] = useState<string[]>([]);
+    const [checkedTasks, setCheckedTasks] = useState<string[]>([]);
 
     const handleAddTask = () => {
     
@@ -39,7 +41,27 @@ export default function Tasks() {
         }
     };
 
-    useEffect(() => {
+    // Add another async storage function for completed tasks
+    const storeCompletedData = async (value) => {
+        try {
+            const jsonValue = JSON.stringify(value);
+            await AsyncStorage.setItem('completed-tasks', jsonValue);
+        } catch (e) {
+            // saving error
+        }
+    };
+
+
+    const handleTaskCompletion = (taskName: string) => {
+        const updatedTasks = tasks.filter(task => task !== taskName);
+        setTasks(updatedTasks);
+        setCheckedTasks(prev => [...prev, taskName]);
+        storeData(updatedTasks);
+        storeCompletedData(checkedTasks);
+    };
+
+    // once at the app launch
+    useEffect(() => {  
         getData().then((data) => { data? setTasks(data):setTasks([])});
     }, [])
 
@@ -60,9 +82,10 @@ export default function Tasks() {
                 keyExtractor={(item, index) => item.toString()}
                 renderItem={({ item }) => (
                     <View style={styles.taskContainer}>
+                        <Checkbox style={styles.checkbox} value={false} onValueChange={() => handleTaskCompletion(item)} />
                         <Text>{item}</Text>
                     </View>
-                )}
+                )}                
             />
         </>
     );
@@ -82,8 +105,14 @@ const styles = StyleSheet.create({
         padding: 5,
     },
     taskContainer: {
+        flexDirection: 'row',  // Set direction of inner items to horizontal
+        alignItems: 'center',  // Vertically align items to the middle
         padding: 10,
         borderBottomColor: '#eee',
-        borderBottomWidth: 1,
+        borderBottomWidth: 5,
     },
+    checkbox: {
+        marginRight: 10,  // space btw. checkbox and task text
+    },
+    
 });
